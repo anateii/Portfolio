3b96b8ffbd524125759c2f6a69a2d6d814649fe7
Ana Matei <anamatei.099@gmail.com>
2022-06-12 11:11:18 +0200

    First setup with Sass

---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>Who</li>
+        <li>What</li>
+        <li>Work</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar

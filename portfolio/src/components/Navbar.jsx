561da2523b007e2c361f0d71df5630f0c1264c40
Ana Matei <anamatei.099@gmail.com>
2022-06-12 14:17:41 +0200

    Added About Page

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import "../styles/Navbar/Navbar.css"
-
+import {Link} from "react-router-dom"
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div>
-        <a href="about">Who</a>
-        <a href="projects">What</a>
-        <a href="contact">Work</a>
-      </div>
+     <Link to="/home"></Link>
+     <div className="bars"></div>
+     <div className="nav-menu">
+<Link to="" className="activeStyle">Who</Link>
+<Link to="" className="activeStyle">What</Link>
+<Link to="" className="activeStyle">Work</Link>
+     </div>
     </div>
   )
 }

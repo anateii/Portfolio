696f5c704c7fdd857f56210a4a9895b539eaf20f
Ana Matei <anamatei.099@gmail.com>
2022-06-23 11:16:54 +0200

    Added react icons

--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import classes from  "./Header.module.scss"
+import {BiMenuAltRight} from "react-icons/bi"
+import {AiOutlineClose} from 'react-icons/ai'
 
 export const Header = () => {
   return (
     
     <header className={classes.header}>
-    <div className={classes.header_content}>
+    <div className={classes.header__content}>
       <h2>logo</h2>
       </div>
-      <nav className={classes.header_content_nav}>
+      <nav className={classes.header__content__nav}>
       <ul>
         <li>Who</li>
         <li>What</li>
         <li>Work</li>
       </ul>
       </nav>
+      <div className={classes.header__content__toggle}></div>
       </header>
     
   )

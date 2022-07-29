d8138c0af823b6398df4354568ef6367b8f3979f
Ana Matei <anamatei.099@gmail.com>
2022-07-29 15:48:32 +0200

    Updates: added typewriter dependency

--- portfolio/src/sections/Home.jsx
+++ portfolio/src/sections/Home.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import classes from './Home.module.scss'
+import Typewriter from 'typewriter-effect'
+
 
 
 export const Home = () => {
   return (
+    
+    <>
     <div className={classes.title}>
         <h1>Hi, I am Ana</h1>
+        
     </div>
+    <Typewriter
+        onInit={(typewriter)=>{
+        typewriter.typeString('React Developer').start() 
+        }} />
+        </>
   )
 }

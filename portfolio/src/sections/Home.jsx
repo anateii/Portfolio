ac7d6333c7c35df42cd747dde00b16e821e1c476
Ana Matei <anamatei.099@gmail.com>
2022-07-29 16:55:30 +0200

    Added Typing Effect

--- portfolio/src/sections/Home.jsx
+++ portfolio/src/sections/Home.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import classes from './Home.module.scss'
-import Typewriter from 'typewriter-effect'
-
-
+import React from "react";
+import classes from "./Home.module.scss";
+import Typewriter from "typewriter-effect";
 
 export const Home = () => {
   return (
-    
     <>
-    <div className={classes.title}>
+      <div className={classes.title}>
         <h1>Hi, I am Ana</h1>
-        
-    </div>
-    <Typewriter
-        onInit={(typewriter)=>{
-        typewriter.typeString('React Developer').start() 
-        }} />
-        </>
-  )
-}
+      </div>
+      <Typewriter
+        /* onInit={(typewriter) => {
+          typewriter
+            .typeString("React Developer")
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString("Funnel Builder")
+            .pauseFor(2000)
+            .deleteAll()
+            .typeString("Content Creator")
+            .start();
+        } 
+      */ options={{
+          strings: [
+            "React Nerd",
+            "Sales Funnel Designer",
+            "IG Content Creator",
+          ],
+          autoStart: true,
+          loop: true,
+        }}
+      />
+    </>
+  );
+};
--- portfolio/src/sections/What.jsx
+++ portfolio/src/sections/What.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const What = () => {
-  return (
-    <div>What</div>
-}
+  return <div>What</div>;
+};

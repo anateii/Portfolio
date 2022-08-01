65de8782e9a0ba142f705f19dad0e4bcd9a7e38c
Ana Matei <anamatei.099@gmail.com>
2022-08-01 13:46:10 +0200

    Fixed Download Button

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,21 +1,20 @@
 import { Header } from "./components/Header/Header";
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { Who } from './sections/Who'
-import {What} from './sections/What'
-import {Work} from './sections/Work'
-import {Home} from './sections/Home'
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Who } from "./sections/Who";
+import { What } from "./sections/What";
+import { Work } from "./sections/Work";
+import { Home } from "./sections/Home";
 
 function App() {
   return (
     <Router>
-     <Header/>
-     <Routes>
-     <Route path="/home" element={<Home/>}/>
-     <Route path="/section-who" element={<Who/>}/>
-     <Route path="/section-what" element={<What/>}/>
-     <Route path="/section-work" element={<Work/>}/>
-     </Routes>
+      <Header />
+      <Home />
+      <Routes>
+        <Route path="/section-who" element={<Who />} />
+        <Route path="/section-what" element={<What />} />
+        <Route path="/section-work" element={<Work />} />
+      </Routes>
     </Router>
   );
 }
--- portfolio/src/components/Header/Header.jsx
+++ portfolio/src/components/Header/Header.jsx
@@ -4,85 +4,90 @@ import { AiOutlineClose } from "react-icons/ai";
 import classes from "./Header.module.scss";
 import { Link, useNavigate } from "react-router-dom";
 
-
-
-
-
 export const Header = () => {
-    const history = useNavigate();
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [size, setSize] = useState({
-        width: undefined,
-        height: undefined,
-    });
+  const history = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
 
-    useEffect(() => {
-        const handleResize = () => {
-            setSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
-        window.addEventListener("resize", handleResize);
-
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    useEffect(() => {
-        if (size.width > 768 && menuOpen) {
-            setMenuOpen(false);
-        }
-    }, [size.width, menuOpen]);
-
-    const menuToggleHandler = () => {
-        setMenuOpen((p) => !p);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
     };
+    window.addEventListener("resize", handleResize);
 
-    const ctaClickHandler = () => {
-        menuToggleHandler();
-        history.push("/page-cta");
-    };
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    return (
-        <header className={classes.header}>
-            <div className={classes.header__content}>
-                <Link to="/home" >
-                   <img src="/assets/logo.png" alt="" className={classes.header__content__logo}/>
-                </Link>
-                <nav
-                    className={`${classes.header__content__nav} ${
-                        menuOpen && size.width < 768 ? classes.isMenu : ""
-                    }`}
-                >
-                    <ul>
-                        <li>
-                            <Link to="/section-who" onClick={menuToggleHandler}>
-                                Who
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/section-what" onClick={menuToggleHandler}>
-                                What
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/section-work" onClick={menuToggleHandler}>
-                               Work
-                            </Link>
-                        </li>
-                    </ul>
-                    <button onClick={ctaClickHandler}>Download Resume</button>
-                </nav>
-                <div className={classes.header__content__toggle}>
-                    {!menuOpen ? (
-                        <BiMenuAltRight onClick={menuToggleHandler} />
-                    ) : (
-                        <AiOutlineClose onClick={menuToggleHandler} />
-                    )}
-                </div>
-            </div>
-        </header>
-    );
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
+  const ctaClickHandler = () => {
+    menuToggleHandler();
+    history.push("/page-cta");
+  };
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.header__content}>
+        <Link to="/">
+          <img
+            src="/assets/logo.png"
+            alt=""
+            className={classes.header__content__logo}
+          />
+        </Link>
+        <nav
+          className={`${classes.header__content__nav} ${
+            menuOpen && size.width < 768 ? classes.isMenu : ""
+          }`}
+        >
+          <ul>
+            <li>
+              <Link to="/section-who" onClick={menuToggleHandler}>
+                Who
+              </Link>
+            </li>
+            <li>
+              <Link to="/section-what" onClick={menuToggleHandler}>
+                What
+              </Link>
+            </li>
+            <li>
+              <Link to="/section-work" onClick={menuToggleHandler}>
+                Work
+              </Link>
+            </li>
+          </ul>
+          <a
+            href="/assets/CV - 2022.pdf"
+            download="CV"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button onClick={ctaClickHandler}>Download Resume</button>
+          </a>
+        </nav>
+        <div className={classes.header__content__toggle}>
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          ) : (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          )}
+        </div>
+      </div>
+    </header>
+  );
 };
-
-

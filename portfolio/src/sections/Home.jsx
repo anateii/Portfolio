bc23beeaeae231682b10a80f33b669140556cd40
Ana Matei <anamatei.099@gmail.com>
2022-08-02 18:22:47 +0200

    Glass effect - Navbar

--- portfolio/src/sections/Home.jsx
+++ portfolio/src/sections/Home.jsx
@@ -4,35 +4,37 @@ import Typewriter from "typewriter-effect";
 
 export const Home = () => {
   return (
-    <div className={classes.titleWrapper}>
-      <div className={classes.title}>
-        <h1>Hi, I am Ana</h1>
+    <>
+      <div className={classes.titleWrapper}>
+        <div className={classes.title}>
+          <h1>Hi, I am Ana</h1>
+        </div>
+        <Typewriter
+          /* onInit={(typewriter) => {
+            typewriter
+              .typeString("React Developer")
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("Funnel Builder")
+              .pauseFor(2000)
+              .deleteAll()
+              .typeString("Content Creator")
+              .start();
+          } 
+        */ options={{
+            strings: [
+              "React Developer",
+              "Sales Funnel Designer",
+              "IG Content Creator",
+            ],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: "natural",
+            pauseFor: 1500,
+          }}
+        />
       </div>
-      <Typewriter
-        /* onInit={(typewriter) => {
-          typewriter
-            .typeString("React Developer")
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString("Funnel Builder")
-            .pauseFor(2000)
-            .deleteAll()
-            .typeString("Content Creator")
-            .start();
-        } 
-      */ options={{
-          strings: [
-            "React Developer",
-            "Sales Funnel Designer",
-            "IG Content Creator",
-          ],
-          autoStart: true,
-          loop: true,
-          deleteSpeed: "natural",
-          pauseFor: 1500,
-        }}
-      />
-      <div>Hello</div>
-    </div>
+      <div>Hello</div>{" "}
+    </>
   );
 };

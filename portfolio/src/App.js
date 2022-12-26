7296c66f23ea7f3d4924cc74d24f2d9cc01c7163
Ana Matei <anamatei.099@gmail.com>
2022-12-26 21:56:54 +0100

    Added Context Api

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,18 +4,29 @@ import { Who } from "./sections/Who";
 import { What } from "./sections/What";
 import { Work } from "./sections/Work";
 import { Home } from "./sections/Home";
+import { createContext, useState } from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  const toggleTheme = () => {
+    setTheme((current) => (current === "light" ? "dark" : "light"));
+  };
+
   return (
-    <Router>
-      <Header />
-      <Home />
-      <Routes>
-        <Route path="/who" element={<Who />} />
-        <Route path="/what" element={<What />} />
-        <Route path="/work" element={<Work />} />
-      </Routes>
-    </Router>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <Router>
+        <Header />
+        <Home />
+        <Routes>
+          <Route path="/who" element={<Who />} />
+          <Route path="/what" element={<What />} />
+          <Route path="/work" element={<Work />} />
+        </Routes>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
--- portfolio/src/sections/What.jsx
+++ portfolio/src/sections/What.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { useState } from "react";
 
 export const What = () => {
-  return <div>What</div>;
+  const [user, setUser] = useState('')
+  const [showUser, setShowUser] = useState(false)
+
+  return <div>
+    <input type='text' placeholder="Say your name ..." onChange={(event) => { setUser(event.target.value) }}></input>
+    <button onClick={() => { setShowUser(true) }}>Click me</button>;
+    {showUser && <h1>{user}</h1>}</div>
 };

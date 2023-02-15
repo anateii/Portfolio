8ea761101d0a4040cd34453802fa2721ecea01b5
Ana Matei <anamatei.099@gmail.com>
2023-02-16 00:28:23 +0100

    HeroSection mobile responsivness

--- portfolio/src/sections/Home.jsx
+++ portfolio/src/sections/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import classes from './Home.module.scss'
 
 export const Home = () => {
   return (
-    <>
-
-    </>
+    <div className={classes.container}>
+      <h3>Hi, my name is </h3>
+      <h2>Ana Matei.</h2>
+      <h1>I create great web experiences.</h1>
+      <p>I’m a FrontEnd Developer specializing in building and designing
+        exceptional digital experiences with modern JS libraries like ReactJS.</p>
+      <button>Let's chat!</button>
+    </div>
   );
 };
+

d6cd66ae2c9ad074f46ffa4d0578a9cdc8824abe
Ana Matei <anamatei.099@gmail.com>
2023-02-25 17:38:09 +0100

    Added Experience Section

--- portfolio/src/components/What.jsx
+++ portfolio/src/components/What.jsx
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from '../styles/What.module.scss'
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import "../styles/tabs.scss"
+import data from "../data.json"
 
 
 export const What = () => {
 
+  const titleVariants = {
+    visible: { opacity: 1, x: 0, transition: { duration: 1 }, },
+    hidden: { opacity: 0, x: -100 }
+  };
 
-  return <div id="what" className={classes.container}></div>
+  const controls = useAnimation();
+
+  const [ref, inView] = useInView();
+
+
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const [activeTab, setActiveTab] = useState(1);
+
+
+  return (
+    <div id="what" className={classes.container}>
+      <motion.div
+        ref={ref}
+        animate={controls}
+        variants={titleVariants}
+        initial="hidden"
+      >
+        <h2 className={classes.container__title}>Where I've worked</h2></motion.div>
+      <div className="tabs-container">
+        <div className="tabs-header">
+          <div
+            className={`tab ${activeTab === 1 ? "active" : ""}`}
+            onClick={() => setActiveTab(1)}
+          >
+            Mooney
+          </div>
+          <div
+            className={`tab ${activeTab === 2 ? "active" : ""}`}
+            onClick={() => setActiveTab(2)}
+          >
+            Premium Expert
+          </div>
+          <div
+            className={`tab ${activeTab === 3 ? "active" : ""}`}
+            onClick={() => setActiveTab(3)}
+          >
+            Freelancer
+          </div>
+        </div>
+        <div className="tabs-content">
+          {data.map((element) => (
+            <div
+              key={element.id}
+              className={`tab-item ${activeTab === element.id ? "active" : ""}`}>
+              <h3>
+                <span>{element.title}</span>
+                {element.id === 1 || element.id === 2 ? (
+                  <>
+                    <span>@</span>
+                    <a href={element.link}>{element.job}</a>
+                  </>) : ('')}
+              </h3>
+              <p>{element.time}</p>
+              <ul>
+                {element.body.split("\n").map((string, index) => (<li key={index}>{string}</li>))}</ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div >)
 };
--- portfolio/src/components/Who.jsx
+++ portfolio/src/components/Who.jsx
@@ -60,8 +60,8 @@ export const Who = () => {
             <div className={classes.container__about__intro}>
               <p>Hello! My name is Ana and I enjoy creating things that live on the internet.
                 My interest in web development started back in 2020 when I decided to dive deep into
-                <span style={{ fontWeight: '800', color: '#fff' }}> Digital Marketing,</span> more specifically
-                into creating <span style={{ fontWeight: '800', color: '#fff' }}>sales funnels</span> for Italian Business Coaches.
+                <span style={{ fontWeight: '800', color: '#b4007e' }}> Digital Marketing,</span> more specifically
+                into creating <span style={{ fontWeight: '800', color: '#b4007e' }}>sales funnels</span> for Italian Business Coaches.
                 Turns out using tools like Clickfunnels and GoHighLevel taught me a lot about HTML & CSS!
               </p>
               <p>Fast-forward to today, and I’ve had the privilege of working as a Sales Funnel Designer, Appointment Setter, Copywriter, and finally
@@ -85,9 +85,7 @@ export const Who = () => {
             initial="hidden"
           >
             <div className={classes.container__about__imgWrapper}>
-              <div className={classes.firstBox}
-                style={myStyle}></div>
-              <div className={classes.secondBox}></div>
+              <div className={classes.firstBox} style={myStyle}></div>
             </div>
           </motion.div>
 

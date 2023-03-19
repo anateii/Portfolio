b8d305db24de7681ea7ef36ba55b857cae5e00b1
Ana Matei <anamatei.099@gmail.com>
2023-03-19 14:57:08 +0100

    3d added - mobile view

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from '../styles/Home.module.scss'
+import VideoBackground from '../spline/VideoBackground'
 
 
 export const Home = () => {
@@ -8,13 +9,16 @@ export const Home = () => {
 
   return (
     <>
-      <div className={classes.container}>
-        <h3>Hi, my name is </h3>
-        <h2 className={classes.name}>Ana Matei.</h2>
-        <h1>I create great web experiences.</h1>
-        <p>I’m a FrontEnd Developer specialized in building and designing
-          exceptional digital experiences with modern JS libraries like ReactJS.</p>
-        <button onClick={() => window.location.href = "mailto:anamateidev@gmail.com"}>LET'S CHAT!</button>
+      <div className={classes.wrapper}>
+        <div className={classes.container}>
+          <h3>Hi, my name is </h3>
+          <h2 className={classes.name}>Ana Matei.</h2>
+          <h1>I create great web experiences.</h1>
+          <p>I’m a FrontEnd Developer specialized in building and designing
+            exceptional digital experiences with modern JS libraries like ReactJS.</p>
+          <button onClick={() => window.location.href = "mailto:anamateidev@gmail.com"}>LET'S CHAT!</button>
+        </div>
+        <VideoBackground className={classes.video} />
       </div>
     </>
 
--- portfolio/src/spline/VideoBackground.jsx
+++ portfolio/src/spline/VideoBackground.jsx
@@ -1,7 +1,9 @@
 import Spline from '@splinetool/react-spline';
-
+import classes from '../styles/Home.module.scss'
 export default function App() {
     return (
-        <Spline scene="https://prod.spline.design/qfFhXlrmqOaTUmNE/scene.splinecode" />
+        <Spline className={classes.spline} scene="https://prod.spline.design/W9dN79cDNZvO2-gv/scene.splinecode"
+        />
     );
 }
+
